b77c81b96bfd618c51d62d0537e4709fad9c02d1
jsmit <jsmit@pictura-dp.nl>
2014-11-28 15:48:54 +0100

    Added first files and interfaces

---
+++ src/Interfaces/Set.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jsmit
+ * Date: 28-11-14
+ * Time: 12:56
+ */
+
+namespace Picturae\OAI\Interfaces;
+
+
+interface Set
+{
+
+    /**
+     * @return string
+     * a colon [:] separated list indicating the path from the root of the set hierarchy to the
+     * respective node. Each element in the list is a string consisting of any valid URI unreserved characters, which
+     * must not contain any colons [:]. Since a setSpec forms a unique identifier for the set within the repository, it
+     * must be unique for each set. Flat set organizations have only sets with setSpec that do not contain any colons.
+     */
+    public function getSpec();
+
+    /**
+     * @return string a short human-readable string naming the set.
+     */
+    public function getName();
+
+    /**
+     * @return string|null
+     * an optional and repeatable container that may hold community-specific XML-encoded data about
+     * the set; the accompanying Implementation Guidelines document provides suggestions regarding the usage of this
+     * container.
+     */
+    public function getDescription();
+}
